b486e220e081a41d022a51aca4119fdb6c28caaf
Alin Enachescu <alinenachescu033@gmail.com>
2019-01-23 23:11:53 +0200

    used async functions

--- extension/background.js
+++ extension/background.js
@@ -3,68 +3,68 @@ let filteredUrls = [
     "http://www.phiorg.ro/*"
 ];
 
-function onBeforeRequestCbk(details) {
-    console.info(`called onBeforeRequestCbk requestId = ${details.requestId}`);
+async function onBeforeRequestCbk(details) {
+    await console.info(`called onBeforeRequestCbk requestId = ${details.requestId}`);
 
     let requestId = details.requestId;
-    let filter = browser.webRequest.filterResponseData(requestId);
-    let decoder = new TextDecoder("utf-8");
-    let encoder = new TextEncoder();
+    let filter = await browser.webRequest.filterResponseData(requestId);
+    let decoder = await new TextDecoder("utf-8");
+    let encoder = await new TextEncoder();
 
     /*
      * send documentUrl, originUrl, url, method and requestBody to native app
      */
 
-    filter.ondata = event => {
-        console.info(`called ondata requestId = ${requestId}`);
+    filter.ondata = async function(event) {
+        await console.info(`called ondata requestId = ${requestId}`);
 
-        let data = decoder.decode(event.data, { stream: true });
-        console.info(`received data on requestId = ${requestId}, data = ${data}`);
+        let data = await decoder.decode(event.data, { stream: true });
+        await console.info(`received data on requestId = ${requestId}, data = ${data}`);
 
         /*
          * send response's body to native app
          */
 
-        console.info(`returned from ondata requestId = ${requestId}`);
+        await console.info(`returned from ondata requestId = ${requestId}`);
     }
 
-    filter.onstop = event => {
-        console.info(`called onstop requestId = ${requestId}`);
+    filter.onstop = async function(event) {
+        await console.info(`called onstop requestId = ${requestId}`);
 
         let data = "modified content";
         /*
          * get response's body from native app
          */
 
-        filter.write(encoder.encode(data));
-        filter.close();
+        await filter.write(encoder.encode(data));
+        await filter.close();
 
-        console.info(`returned from onstop requestId = ${requestId}`);
+        await console.info(`returned from onstop requestId = ${requestId}`);
     }
 
-    console.info(`returned from onBeforeRequestCbk requestId = ${requestId}`);
+    await console.info(`returned from onBeforeRequestCbk requestId = ${requestId}`);
     return {};
 }
 
-function onBeforeSendHeadersCbk(details) {
-    console.info(`called onBeforeSendHeadersCbk requestId = ${details.requestId}`);
+async function onBeforeSendHeadersCbk(details) {
+    await console.info(`called onBeforeSendHeadersCbk requestId = ${details.requestId}`);
 
     /*
      * send request's headers to native app and wait for response
      */
 
-    console.info(`returned from onBeforeSendHeadersCbk requestId = ${details.requestId}`);
+    await console.info(`returned from onBeforeSendHeadersCbk requestId = ${details.requestId}`);
     return { requestHeaders: details.requestHeaders };
 }
 
-function onHeadersReceivedCbk(details) {
-    console.info(`called onHeadersReceivedCbk requestId = ${details.requestId}`);
+async function onHeadersReceivedCbk(details) {
+    await console.info(`called onHeadersReceivedCbk requestId = ${details.requestId}`);
 
     /*
      * send statusLine and response's headers to native app and wait for response
      */
 
-    console.info(`returned from onHeadersReceivedCbk requestId = ${details.requestId}`);
+    await console.info(`returned from onHeadersReceivedCbk requestId = ${details.requestId}`);
     return { responseHeaders: details.responseHeaders };
 }
 
